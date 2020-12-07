d7de5f995cf44db980dc428b0b8541b92e29bf5e
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2020-12-07 20:35:00 +0000

    Calculate age with name, birth year, and current year

---
+++ age-calculator.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function ageCalculator(name, yearOfBirth, currentYear) {
+  return name + " is " + (currentYear - yearOfBirth) + " years old.";
+}
+
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
+console.log(ageCalculator("Jack", 2004, 2016));
+console.log(ageCalculator("Ali", 2016, 2016));
