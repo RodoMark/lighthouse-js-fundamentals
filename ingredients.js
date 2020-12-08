e4dafaa3975d458d647c46a7d77fcea3170296bf
Alon Rodovinsky <alon.rodovinsky@gmail.com>
2020-12-08 00:13:21 +0000

    3 loops that print out an array of ingredients

---
+++ ingredients.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const ingredients = [
+  "eggs",
+  "milk",
+  "flour",
+  "sugar",
+  "baking soda",
+  "baking powder",
+  "chocolate chips",
+  "bananas",
+];
+
+// Write a while loop that prints out the contents of ingredients:
+let i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
+
+// Write a for loop that prints out the contents of ingredients:
+for (i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+
+let i = ingredients.length - 1;
+while (i > -1) {
+  console.log(ingredients[i]);
+  i--;
+}
